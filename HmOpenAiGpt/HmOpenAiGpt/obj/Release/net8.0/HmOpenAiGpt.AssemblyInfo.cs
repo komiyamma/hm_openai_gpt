5c9af65921f8d19ea86ab7fe52232e9c11e80cff
komiyamma <komiyama0629@gmail.com>
2024-06-25 00:27:51 +0900

    全リセット時はウィンドウをとじておくべきだ

--- HmOpenAiGpt/HmOpenAiGpt/obj/Release/net8.0/HmOpenAiGpt.AssemblyInfo.cs
+++ HmOpenAiGpt/HmOpenAiGpt/obj/Release/net8.0/HmOpenAiGpt.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2024 @ Akitsugu Komiyama")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.1.7")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+1448633a503df3d2ef48528e3e3f6154f41c9c61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.1.7+85f47008de0a77aa158087d66dbbe53fe30aeff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HmOpenAiGpt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HmOpenAiGpt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.1.7")]
